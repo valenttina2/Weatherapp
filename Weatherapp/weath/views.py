1350c6591461da6e7b6bb6623b5d17a185629ee3
Andrei <valuschika@gmail.com>
2022-08-30 14:56:01 +0300

    url, res

--- Weatherapp/weath/views.py
+++ Weatherapp/weath/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid='ce353e843fb5c485d1458e8a13f32132'
+    url="https://api.openweathermap.org/data/2.5/weather?q= {}&units=metric&appid="+appid
+    city='Minsk'
+    res=requests.get(url.format(city))
+    print(res.text)
     return render(request, 'weath/index.html')
