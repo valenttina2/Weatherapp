a47e44faf3d8bdd30d117cf25fb31b0655ca7fe0
Andrei <valuschika@gmail.com>
2022-08-31 22:53:15 +0300

    url, res

--- Weatherapp/weath/views.py
+++ Weatherapp/weath/views.py
@@ -5,6 +5,15 @@ def index(request):
     appid='ce353e843fb5c485d1458e8a13f32132'
     url="https://api.openweathermap.org/data/2.5/weather?q= {}&units=metric&appid="+appid
     city='Minsk'
-    res=requests.get(url.format(city))
-    print(res.text)
-    return render(request, 'weath/index.html')
+    res=requests.get(url.format(city)).json()
+    city_info={
+        'city':city,
+        'temp':res["main"]["temp"],
+        'wind':res["wind"]["speed"],
+        'icon':res["weather"][0]["icon"],
+        'visibility':res["visibility"]
+    }
+
+    context={'info':city_info}
+
+    return render(request, 'weath/index.html', context)
